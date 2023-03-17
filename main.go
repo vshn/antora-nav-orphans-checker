06bf5d515e0ab9710d7e8df4a00859bf13679bfb
Adrian Kosmaczewski <adrian.kosmaczewski@vshn.ch>
2023-03-17 11:42:57 +0100

    Added a test

--- main.go
+++ main.go
@@ -21,6 +21,24 @@ func main() {
 		path = strings.TrimSuffix(path, "/")
 	}
 
+	// Main block
+	errors := check(path)
+
+	// If there are errors, print the list of orphan files
+	if len(errors) > 0 {
+		for _, file := range errors {
+			fmt.Println(fmt.Sprintf("File '%s' not in 'nav.adoc'", file))
+		}
+		os.Exit(1)
+	}
+
+	// No errors, all good!
+	fmt.Println("No orphan files in modules/ROOT/nav.adoc")
+	os.Exit(0)
+
+}
+
+func check(path string) []string {
 	// We assume that the project follows a standard Antora layout
 	startPath := path + "/modules/ROOT/pages"
 	allFiles, err := listAllFiles(startPath)
@@ -43,37 +61,24 @@ func main() {
 	// Verify that all filtered files appear in the nav.adoc file at least once
 	navPath := path + "/modules/ROOT/nav.adoc"
 	regex := `xref:(.+)\[`
-	errors := check(navPath, filtered, regex)
-
-	// If there are errors, print the list of orphan files
-	if len(errors) > 0 {
-		for _, v := range errors {
-			fmt.Println(v)
-		}
-		os.Exit(1)
-	}
-
-	// No errors, all good!
-	fmt.Println("No orphan files in modules/ROOT/nav.adoc")
-	os.Exit(0)
+	errors := walk(navPath, filtered, regex)
+	return errors
 }
 
 // Checks the file nav.adoc for the existence of at least one
 // reference to each one of the files passed in the second parameter,
 // using the regular expresssion as parameter.
-func check(navPath string, files []string, regex string) []string {
+func walk(navPath string, files []string, regex string) []string {
 	var errors []string
 	re := regexp.MustCompile(regex)
 	contents, err := os.ReadFile(navPath)
 	if err == nil {
 		stringContents := string(contents)
 		matches := re.FindAllString(stringContents, -1)
-		for _, file := range files {
+		errors = filterArray(files, func(file string) bool {
 			match := fmt.Sprintf("xref:%s[", file)
-			if !stringInSlice(match, matches) {
-				errors = append(errors, fmt.Sprintf("File '%s' not in 'nav.adoc'", file))
-			}
-		}
+			return !stringInSlice(match, matches)
+		})
 	} else {
 		errors = append(errors, "Cannot read file "+navPath)
 	}
