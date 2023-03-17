ff9f3ad49b278d9221fd7e511a7b81d54426b0f8
Adrian Kosmaczewski <adrian.kosmaczewski@vshn.ch>
2023-03-17 11:44:06 +0100

    Refactored test

--- main_test.go
+++ main_test.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestShouldFail(t *testing.T) {
-	path := "fixture"
-	errors := check(path)
+	errors := check("fixture")
 
 	if len(errors) == 1 {
 		error := errors[0]
-		fmt.Println(error)
 		if error != "third.adoc" {
 			t.Fail()
 		}
