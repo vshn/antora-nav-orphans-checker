dad2ff640d69f651b6c46c15bfc45b221ce92c33
Adrian Kosmaczewski <adrian.kosmaczewski@vshn.ch>
2023-03-20 09:08:03 +0100

    Added support for other modules and other documents

--- main_test.go
+++ main_test.go
@@ -4,12 +4,38 @@ import (
 	"testing"
 )
 
-func TestShouldFail(t *testing.T) {
-	errors := check("fixture")
+func TestShouldFailNavInROOT(t *testing.T) {
+	errors := check("fixture", "ROOT", "/modules/ROOT/nav.adoc")
 
 	if len(errors) == 1 {
 		error := errors[0]
 		if error != "third.adoc" {
+			t.Fail()
+		}
+	} else {
+		t.Fail()
+	}
+}
+
+func TestShouldFailNavInAnotherModule(t *testing.T) {
+	errors := check("fixture", "AnotherModule", "/modules/AnotherModule/nav.adoc")
+
+	if len(errors) == 1 {
+		error := errors[0]
+		if error != "one.adoc" {
+			t.Fail()
+		}
+	} else {
+		t.Fail()
+	}
+}
+
+func TestShouldFailDocument(t *testing.T) {
+	errors := check("fixture", "ROOT", "/document.adoc")
+
+	if len(errors) == 1 {
+		error := errors[0]
+		if error != "second.adoc" {
 			t.Fail()
 		}
 	} else {
